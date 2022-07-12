015d04ed6033a3255dfba24a9c63fce8fbee2ff7
Petrova Valeria <pristavkovaleriavladimirovna@gmail.com>
2022-07-12 19:29:14 +0300

    adaptive correction

--- dist/index.5d9dacde.js
+++ dist/index.5d9dacde.js
@@ -532,6 +532,59 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"1Z4Rq":[function(require,module,exports) {
+"use strict";
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    Blackberry: function() {
+        return navigator.userAgent.match(/Blackberry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return isMobile.Android() || isMobile.Blackberry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+    }
+};
+if (isMobile.any()) document.body.classList.add("_touch");
+else document.body.classList.add("_pc");
+const iconMenu = document.querySelector(".icon");
+const menuBody = document.querySelector(".menu__body");
+if (iconMenu) iconMenu.addEventListener("click", function(e) {
+    // document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+});
+const menuLinks = document.querySelectorAll(".menu__item_link[data-goto]");
+if (menuLinks.length > 0) {
+    menuLinks.forEach((menuLink)=>{
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector("header").offsetHeight;
+            if (iconMenu.classList.contains("_active")) {
+                document.body.classList.remove("_lock");
+                iconMenu.classList.remove("_active");
+                menuBody.classList.remove("_active");
+            }
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
 
 },{}]},["gzp3I","1Z4Rq"], "1Z4Rq", "parcelRequire2c60")
 
